89fe2047a3c3cf0cdd444ff3f5d27736d446daf7
GuilhermeFontanella <65752491+GuilhermeFontanella@users.noreply.github.com>
2025-08-21 16:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const { mouse, Point } = require("@nut-tree-fork/nut-js");
+const chalk = require("chalk");
+
+const INTERVALO = 180; // segundos (3 min)
+
+async function moverMouse() {
+  const pos = await mouse.getPosition();
+  await mouse.move([new Point(pos.x + 10, pos.y)]);
+  await mouse.move([new Point(pos.x, pos.y)]);
+  console.log("\nMouse movido!");
+}
+
+function gerarBarraProgresso(tempoTotal, tempoRestante, tamanho = 30) {
+  const completado = Math.round(((tempoTotal - tempoRestante) / tempoTotal) * tamanho);
+  const vazio = tamanho - completado;
+  return `[${"=".repeat(completado)}${"-".repeat(vazio)}]`;
+}
+
+// Função para formatar uso de memória em MB
+function formatarMemoria(bytes) {
+  return (bytes / 1024 / 1024).toFixed(2);
+}
+
+
+async function iniciar() {
+  let contador = INTERVALO;
+
+  setInterval(async () => {
+    if (contador === 0) {
+      await moverMouse();
+      contador = INTERVALO;
+    } else {
+      contador--;
+    }
+
+    // Mostra contador, memória e CPU
+    const memoria = formatarMemoria(process.memoryUsage().rss);
+
+    process.stdout.cursorTo(0);
+    process.stdout.write(
+      `Próxima ação do mouse em: ${chalk.green(contador)} segundos... | Uso de memória:  ${memoria > 55 ? chalk.red(memoria) : memoria >= 50 ? chalk.yellow(memoria) : chalk.green(memoria)} MB | Para encerrar pressione Ctrl + C`
+    );
+  }, 1000);
+}
+
+iniciar();
